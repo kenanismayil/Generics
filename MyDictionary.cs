70a47386dcce543c2701fe7909c9d149ef888d8d
keno1907 <63511659+keno1907@users.noreply.github.com>
2021-01-23 03:17:08 +0300

    Initial commit

---
+++ MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class MyDictionary<K>
+    {
+        K[] workers;
+        public MyDictionary()
+        {
+            workers = new K[0];
+        }
+
+        public void Add(K worker)
+        {
+            K[] tempworkers = workers;
+            workers = new K[workers.Length + 1];
+
+            for (int i = 0; i < tempworkers.Length; i++)
+            {
+                workers[i] = tempworkers[i];
+            }
+
+            workers[workers.Length - 1] = worker;
+        }
+
+        public K[] Workers
+        {
+            get { return workers; }
+        }
+
+        public int Length
+        {
+            get { return workers.Length; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> employee = new MyDictionary<string>();
+            employee.Add("Ahmet Ucar");
+            employee.Add("Hatice Yilmaz");
+            employee.Add("Sila Gunel");
+            employee.Add("Engin Demirog");
+            employee.Add("Kenan Ismayil");
+
+            foreach (var worker in employee.Workers)
+            {
+                Console.WriteLine(worker);
+            }
+
+            Console.WriteLine(employee.Length);
+
+
+            Console.ReadLine();
+        }
+    }
+}
